2fb6d8bd68a67ede8d964f5606a2160bfae1ed1e
mir_faisal <mirfaisal08@gmail.comm>
2023-10-22 21:48:05 +0600

    trying to fix background image in live server

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern-mobile": "url('./public/background/for_mobile.png')",
-        "hero-pattern-desktop": "url('./public/background/for_desktop.png')",
+        "hero-pattern-mobile": "url('../public/background/for_mobile.png')",
+        "hero-pattern-desktop": "url('../public/background/for_desktop.png')",
       },
     },
   },
