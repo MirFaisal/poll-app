6e9726604b0fb56b779bbcf562fb971d456a376c
mir_faisal <mirfaisal08@gmail.comm>
2023-10-24 13:43:38 +0600

    redirect problem fix

--- src/Components/PremiumSubCard/PremiumSubCard.jsx
+++ src/Components/PremiumSubCard/PremiumSubCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const PremiumSubCard = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="max-w-sm p-4 sm:p-6 bg-white border border-gray-200 rounded-md shadow-sm">
+      <div className="w-full p-4 sm:p-6 bg-white border border-gray-200 rounded-md shadow-sm">
         <h5 className="mb-2 sm:mb-3 text-2xl text-center font-bold tracking-tight text-gray-900 ">
           Become a Premium subscriber
         </h5>
