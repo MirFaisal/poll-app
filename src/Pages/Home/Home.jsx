0f773cc21ed91e6ed24c933119cc0c040e8a0c59
Nayan-Islam <islamnayan588@gmail.com>
2023-11-04 21:03:12 +0600

    comment and nav fixed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import PostCard from "../../Components/PostCard/PostCard";
 const Home = () => {
   return (
     <div>
-      <div className="md:pl-[240px] pt-28">
+      <div className="pt-28">
         <PostCard />
         <PostCard />
         <PostCard />
